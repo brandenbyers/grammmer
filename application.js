0640191ee4254007cc2f5ed55cf0c42afc5f4474
brndnb <branden@fermup.com>
2015-02-16 16:27:16 -0600

    Add all measurement abbreviations

--- application.js
+++ application.js
@@ -27,7 +27,8 @@
 
   function renderAllTodos(todos) {
     var html = '';
-    todos.forEach(function(todo) {
+    console.log(todos.length);
+    todos.reverse().forEach(function(todo) {
       html += todoToHtml(todo);
     });
     ul.innerHTML = html;
@@ -58,24 +59,79 @@
     if (itemFluidCheck[1] === 'fluid' || itemFluidCheck[1] === 'fl' || itemFluidCheck[1] === 'fl.') {
       unitInput = 'floz';
       ingredientInput = itemFluidCheck.slice(3).join(' ');
+    } else if (str.split(' ')[1] === 'T') {
+      unitInput = 'T',
+      ingredientInput = itemInput.join(' ');
     } else {
       unitInput = itemInput.shift(),
       ingredientInput = itemInput.join(' ');
     }
 
     var unitToAbbreviations = [
-      { 'unit': 'fluid ounces', 'abbreviation': 'floz' },
-      { 'unit': 'fluid ounce', 'abbreviation': 'floz' },
+      { 'unit': 't', 'abbreviation': 't' },
+      { 'unit': 't.', 'abbreviation': 't' },
+      { 'unit': 'tsp', 'abbreviation': 't' },
+      { 'unit': 'tsp.', 'abbreviation': 't' },
+      { 'unit': 'tsps', 'abbreviation': 't' },
+      { 'unit': 'teas', 'abbreviation': 't' },
+      { 'unit': 'teas.', 'abbreviation': 't' },
+      { 'unit': 'teaspoon', 'abbreviation': 't' },
+      { 'unit': 'teaspoons', 'abbreviation': 't' },
+      { 'unit': 'T', 'abbreviation': 'T' },
+      { 'unit': 'tbsp', 'abbreviation': 'T' },
+      { 'unit': 'tbsp.', 'abbreviation': 'T' },
+      { 'unit': 'tbsps', 'abbreviation': 'T' },
+      { 'unit': 'tablespoon', 'abbreviation': 'T' },
+      { 'unit': 'tablespoons', 'abbreviation': 'T' },
+      { 'unit': 'floz', 'abbreviation': 'floz' },
       { 'unit': 'fl oz', 'abbreviation': 'floz' },
       { 'unit': 'fl. oz', 'abbreviation': 'floz' },
-      { 'unit': 'floz', 'abbreviation': 'floz' },
-      { 'unit': 'cups', 'abbreviation': 'c' },
-      { 'unit': 'cup', 'abbreviation': 'c' },
+      { 'unit': 'fluid ounce', 'abbreviation': 'floz' },
+      { 'unit': 'fluid ounces', 'abbreviation': 'floz' },
       { 'unit': 'c', 'abbreviation': 'c' },
-      { 'unit': 'pints', 'abbreviation': 'pt' },
-      { 'unit': 'pint', 'abbreviation': 'pt' },
-      { 'unit': 'pt', 'abbreviation': 'pt' },
+      { 'unit': 'c.', 'abbreviation': 'c' },
+      { 'unit': 'cup', 'abbreviation': 'c' },
+      { 'unit': 'cups', 'abbreviation': 'c' },
       { 'unit': 'p', 'abbreviation': 'pt' },
+      { 'unit': 'p.', 'abbreviation': 'pt' },
+      { 'unit': 'pt', 'abbreviation': 'pt' },
+      { 'unit': 'pt.', 'abbreviation': 'pt' },
+      { 'unit': 'pint', 'abbreviation': 'pt' },
+      { 'unit': 'pints', 'abbreviation': 'pt' },
+      { 'unit': 'q', 'abbreviation': 'qt' },
+      { 'unit': 'q.', 'abbreviation': 'qt' },
+      { 'unit': 'qt', 'abbreviation': 'qt' },
+      { 'unit': 'qt.', 'abbreviation': 'qt' },
+      { 'unit': 'quart', 'abbreviation': 'qt' },
+      { 'unit': 'quarts', 'abbreviation': 'qt' },
+      { 'unit': 'gal', 'abbreviation': 'gal' },
+      { 'unit': 'gal.', 'abbreviation': 'gal' },
+      { 'unit': 'gallon', 'abbreviation': 'gal' },
+      { 'unit': 'lb', 'abbreviation': 'lb' },
+      { 'unit': 'lb.', 'abbreviation': 'lb' },
+      { 'unit': 'lbs', 'abbreviation': 'lb' },
+      { 'unit': 'pound', 'abbreviation': 'lb' },
+      { 'unit': 'pounds', 'abbreviation': 'lb' },
+      { 'unit': 'oz', 'abbreviation': 'oz' },
+      { 'unit': 'oz.', 'abbreviation': 'oz' },
+      { 'unit': 'ozs', 'abbreviation': 'oz' },
+      { 'unit': 'ozes', 'abbreviation': 'oz' },
+      { 'unit': 'ounce', 'abbreviation': 'oz' },
+      { 'unit': 'ounces', 'abbreviation': 'oz' },
+      { 'unit': 'ml', 'abbreviation': 'ml' },
+      { 'unit': 'mls', 'abbreviation': 'ml' },
+      { 'unit': 'milli', 'abbreviation': 'ml' },
+      { 'unit': 'milliliter', 'abbreviation': 'ml' },
+      { 'unit': 'milliliters', 'abbreviation': 'ml' },
+      { 'unit': 'dl', 'abbreviation': 'dl' },
+      { 'unit': 'deciliter', 'abbreviation': 'dl' },
+      { 'unit': 'deciliters', 'abbreviation': 'dl' },
+      { 'unit': 'l', 'abbreviation': 'l' },
+      { 'unit': 'liter', 'abbreviation': 'l' },
+      { 'unit': 'liters', 'abbreviation': 'l' },
+      { 'unit': 'g', 'abbreviation': 'g' },
+      { 'unit': 'gram', 'abbreviation': 'g' },
+      { 'unit': 'grams', 'abbreviation': 'g' }
     ];
     var unitToMilliliters = [
       { 'unit': 'ml', 'conversion': 1 },
@@ -87,7 +143,9 @@
       { 'unit': 'c', 'conversion': 236.6 },
       { 'unit': 'pt', 'conversion': 473 },
       { 'unit': 'qt', 'conversion': 946.4 },
-      { 'unit': 'gal', 'conversion': 3785.4 }
+      { 'unit': 'gal', 'conversion': 3785.4 },
+      { 'unit': 'lb', 'conversion': 453.6 },
+      { 'unit': 'oz', 'conversion': 28.3 }
     ];
     var ingredients = [
       // FLOURS
@@ -113,7 +171,20 @@
     var unitAbrev = _.result(_.find(unitToAbbreviations, { 'unit': unitInput }), 'abbreviation');
     var unitInMill = _.result(_.find(unitToMilliliters, { 'unit': unitAbrev  }), 'conversion');
 
-    var result = Math.round(quantityInput * unitInMill * ingredientDensity) + ' grams ' + ingredientInput;
+    // If already submited as grams, return original weight
+    if (unitAbrev === 'g') {
+      return str.toLowerCase();
+    }
+
+    // If lbs or oz, straight conversion by weight
+    if (unitAbrev === 'lb' || unitAbrev === 'oz') {
+      return Math.round(quantityInput * unitInMill) + ' grams ' + ingredientInput  + ' = ' + input.value;
+    }
+
+    // Convert from volume to weight by density
+    var result = Math.round(quantityInput * unitInMill * ingredientDensity) + ' grams ' + ingredientInput  + ' = ' + input.value;
+
+    // Check for errors
     var errorCheck = result.split(' ');
     if (errorCheck[0] === 'NaN') {
       return 'Incorrect Input. Please Try Again.';
